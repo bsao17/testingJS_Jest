8ba1a9bb10cbd648ac3ab8701c5a28abb9b1e496
bsao17 <declic62@gmail.com>
2022-02-14 17:55:27 +0100

    add __tests__ directory and whole demo test files

--- __tests__/demo.test.js
+++ __tests__/demo.test.js
@@ -1,3 +1,11 @@
-test("sum", ()=>{
-    expect(3+2).toEqual(5)
+
+describe("demo test", ()=>{
+    test("sum", ()=>{
+        expect(3+2).toEqual(5)
+    })
+
+    test("less", ()=>{
+        expect(3-2).toBe(1)
+    })
+
 })
